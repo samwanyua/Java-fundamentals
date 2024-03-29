f6eade69e27df0f88f59e455a777f078401e02b4
samwanyua <samexample8@gmail.com>
2024-03-30 02:36:20 +0300

    Variables

---
+++ Fundamentals/src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+        int num1 = -5;
+
+        System.out.println(num1);
+        System.out.println(2342 + 23489);
+
+    }
+}
