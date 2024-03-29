7c24e9823d5fc72fef99683e5671608b7be706f5
samwanyua <samexample8@gmail.com>
2024-03-30 02:39:18 +0300

    Variables

--- Fundamentals/src/Variables.java
+++ Fundamentals/src/Variables.java
@@ -2,9 +2,13 @@ public class Variables {
     public static void main(String[] args) {
         // Variables
         int num1 = -5;
+        int num2 = 23423;
+
+        int result = num1 + num2;
 
         System.out.println(num1);
         System.out.println(2342 + 23489);
+        System.out.println(result);
 
     }
 }
