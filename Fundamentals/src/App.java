d14b4684c1c8c75a123da193982e3fe739a0db13
samwanyua <samexample8@gmail.com>
2024-03-30 02:02:23 +0300

    Introduction to jshell

---
+++ Fundamentals/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Let's begin with Java");
+    }
+}
+
+// Working with jshell
+/*
+ * Just type `jshell` in the terminal
+ * compiling `javac <filename>` 
+ */
