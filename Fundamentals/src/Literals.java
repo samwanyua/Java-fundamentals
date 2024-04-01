bb7fe3a84f25c6cb75b5151483a71d31b3b78b5c
samwanyua <samexample8@gmail.com>
2024-04-01 22:18:04 +0300

    Data types in Java

---
+++ Fundamentals/src/Literals.java
@@ -0,0 +1,26 @@
+public class Literals{
+    public static void main(String[] args) {
+        // Data types
+        /*
+         * Primitives data types (Basic):
+            * Int : byte(1 byte), short(2 bytes), int(4 bytes), long(8 bytes)
+            * Float: double(8 bytes - default in Java), float(4 bytes - default in C language)
+            * Char : 2 bytes (1 byte in c/c++ language)
+            * Boolean
+         * 
+         * 
+         */
+        int num1 = 9;
+        byte by = 127;
+        short sh = 559;
+        long l = 3242l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = '8';
+        
+        boolean b = true;
+
+    }
+}
